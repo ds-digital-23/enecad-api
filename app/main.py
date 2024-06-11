901df6b2ff3585149b26afdad8a2a12bf44b6d1c
alissonpina <admin@alissonpina.com>
2024-06-11 09:59:58 -0300

    first commit

---
+++ app/main.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from core.configs import settings
+from api.v1.api import api_router
+
+
+
+app = FastAPI(
+    title='Enecad - API',
+    version='1.0.0',
+    description='API desenvolvida para a Enecad a fim de detectar objetos em rede de distribuição via modelos de visão computacional.',
+    license="Licença Comercial Enecad"
+)
+
+
+@app.get("/")
+def health_check():
+    content = {"mensagem": "Bem-vindo à Enecad-API para detecção de objetos em rede de distribuição!"}
+    return JSONResponse(content=content, media_type="application/json; charset=utf-8")
+
+
+app.include_router(api_router, prefix=settings.API_VERSION)

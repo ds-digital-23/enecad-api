d62d84a0179bcdf3edc2528ac16ca292fd952a18
ds-digital-23 <DSWeb@dronessolution.com.br>
2024-06-13 23:08:23 -0300

    Try fix models folder

--- app/api/v1/endpoints/solicitacao.py
+++ app/api/v1/endpoints/solicitacao.py
@@ -17,7 +17,7 @@ from schemas.modelo_schema import ModeloResponse
 from schemas.solicitacao_schema import SolicitacaoCreate, PolesRequest, Resultado
 from core.deps import get_session, get_current_user
 
-logging.getLogger('ultralytics').setLevel(logging.ERROR)
+#logging.getLogger('ultralytics').setLevel(logging.ERROR)
 
 router = APIRouter()
 semaphore = asyncio.Semaphore(5)
@@ -27,7 +27,7 @@ loaded_models = {}
 
 async def get_model(modelo: ModeloModel):
     if modelo.modelo_nome not in loaded_models:
-        model_path = os.path.join('ia', modelo.modelo_nome)
+        model_path = os.path.join('app', 'ia', modelo.modelo_nome)
         model_ia = await asyncio.to_thread(YOLO, model_path)
         loaded_models[modelo.modelo_nome] = model_ia
     return loaded_models[modelo.modelo_nome]

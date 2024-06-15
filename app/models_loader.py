b9fc1614a631d7075dacf8bfea65d97f91f058e9
ds-digital-23 <DSWeb@dronessolution.com.br>
2024-06-14 21:45:45 -0300

    Fix models_loader extension

--- app/models_loader.py
+++ app/models_loader.py
@@ -6,7 +6,7 @@ loaded_models = {}
 
 async def load_models():
     model_directory = 'ia'  # Diretório onde os modelos estão armazenados
-    model_files = [f for f in os.listdir(model_directory) if os.path.isfile(os.path.join(model_directory, f))]
+    model_files = [f for f in os.listdir(model_directory) if f.endswith('.pt') and os.path.isfile(os.path.join(model_directory, f))]
 
     for model_file in model_files:
         model_path = os.path.join(model_directory, model_file)
